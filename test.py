7e0e11699994cc2ab6f6bc0f4e3a7549313fd156
Will Gautreaux <28786751+Runninoutoftime@users.noreply.github.com>
2022-11-13 19:37:17 -0500

    image stitching implemented

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import google_streetview.api
 import google_streetview.helpers
+from dotenv import load_dotenv
+from PIL import Image
+from imutils import paths
+import numpy as np
+import cv2
+
 
 # Define parameters for street view api
 params = {
     'size': '640x640', # max 640x640 pixels
     'location': '33.933260, -83.382731',
-    'heading': '0;90;180;270',
-    'pitch': '-0.76',
+    'heading': '0;60;120;180;240;300',
+    'pitch': '0',
     'fov': '90',
-    'key': 'key_here',
+    'key': 'AIzaSyA1rcyAoZTClEYgNTvkvv8mHKUIsnVMC-U',
     }
 
 # Create a results object
@@ -20,15 +26,32 @@ results = google_streetview.api.results(api_list)
 
 results.preview()
 
+# For each result in results, check if date is in range
+
 results.download_links('images')
 
 results.save_metadata('metadata')
 
-# google_streetview.api.
 
-# results.preview()
+def stitch_images(): 
 
-# Download images to directory 'downloads'
-# results.download_links('downloads')
+    imagePaths = sorted(list(paths.list_images("images")))
+    images = []
 
+    print(imagePaths)
+    for imagePath in imagePaths:
+        image = cv2.imread(imagePath)
+        images.append(image)
 
+    stitcher = cv2.Stitcher_create()
+    (status, stitched) = stitcher.stitch(images)
+
+    if status == 0:
+        cv2.imwrite("output.png", stitched)
+
+        cv2.imshow("Stitched", stitched)
+        cv2.waitKey('q')
+    else:
+        print("error with stitching")
+
+stitch_images()
